377bafba75d93695bcc2d85f66c75fe416a0ea59
Daniyal-Afzaal1 <daniyalafzaal@yahoo.com>
2026-02-09 19:38:18 +0500

    Started procedural version, added compatibility with linux and windows

--- src/main.cpp
+++ src/main.cpp
@@ -5,45 +5,49 @@
     namespace fs = std::filesystem; //creates an alias
 
     int main(){
-        fs::path Downloads = "/home/btw/Downloads";
-        for(const auto& entry : fs::directory_iterator(Downloads)){
-           if(entry.is_directory()){ //skips directories
-            continue;
-           }
+      int choice;
+      do{
+      cout << R"(     _   _   _ _____ ___        n_____ ___ _     _____       ___  ____   ____    _    _   _ ___ __________ ____  
+    / \ | | | |_   _/ _ \      |  ___|_ _| |   | ____|     / _ \|  _ \ / ___|  / \  | \ | |_ _|__  / ____|  _ \ 
+   / _ \| | | | | || | | |_____| |_   | || |   |  _| _____| | | | |_) | |  _  / _ \ |  \| || |  / /|  _| | |_) |
+  / ___ \ |_| | | || |_| |_____|  _|  | || |___| |__|_____| |_| |  _ <| |_| |/ ___ \| |\  || | / /_| |___|  _ < 
+ /_/   \_\___/  |_| \___/      |_|   |___|_____|_____|     \___/|_| \_\\____/_/   \_\_| \_|___/____|_____|_| \_\
+                                                                                                                )";
 
-           fs::path filePath = entry.path();
-           string ext = filePath.extension();
+    cout<<endl;
+    cout<<"-----------------------------------------------------------------------------------------------------------------"<<endl;
+    cout<<"          MENU"<<endl;
+    cout<<"1. Organize the Files"<<endl;
+    cout<<"2. Exit"<<endl; 
+    cout<<"Enter your choice: "<<endl;
+    cin>>choice;
+    cin.ignore();
+    if(choice == 1){
+      string Spath;
+      fs::path Path;
+      cout<<"Copy or Enter the Path where you want to oraganize files"<<endl;
+      getline(cin,Spath); // in string as path can have space 
+      // Path = Spath;
+      if(Spath.)
+      if(!fs::exists(Path) || !fs::is_directory(Path)){
+        cout<<"Path does not exits or is not a folder!"<<endl;
+        continue;
+      }
+      else{
+        cout<<"Organizing files in: "<< Path <<endl;
+      }
+      
 
-           fs::path destination;
-
-           // Decides folder based on extension
-           if(ext == ".pdf"){
-            destination = Downloads/"PDFs";
-           }
-           else if(ext == ".jpg" || ext == ".png"){
-            destination = Downloads/"Images";
-           }
-           else if(ext == ".mp4" || ext == ".mkv"){
-            destination = Downloads/"Videos";
-           }
-           else{
-            continue; //ignores other files
-           }
-
-           //Create folder if it not exists
-           if(!fs::exists(destination)){
-            fs::create_directory(destination);
-           }
-
-           //new file Location
-           fs::path newLocation = destination/filePath.filename();
-
-           //Move file
-           if(!fs::exists(newLocation)){
-           fs::rename(filePath, newLocation);
-        }
-
-           cout<<"Moved "<<filePath.filename()<<endl;
-        }
-        return 0;
-}
+    }
+    else if(choice == 2){
+      cout<<"Quitting...."<<endl;
+    }
+    else{
+      cout<<"- Enter correct number from the menu !"<<endl;
+      continue;
+    }                                                                                                         
+      
+      }while(choice!=2);
+      
+      
+    }
